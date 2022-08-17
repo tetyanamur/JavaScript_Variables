555d76f6b892cb7aec33040cacc0b51d6b0c8fed
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-17 15:41:42 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
+
+
 console.log("Hello World!\n==========\n");
 console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
 // Exercise 1
-
+const FirstName = "Tetyana";
+let LastName = "Murzina";
+var myAge = 24.45;
+let isDone;
 // Exercise 2
+let fullName = FirstName + " " + LastName;
+let templatefullName = `${FirstName} ${LastName}`;
+console.log("fullname: ", fullName)
+console.log("templatefullName:", templatefullName);
 
 // Exercise 3
+let myStory
+let myCity = "Edmond"
+let pastTime = "read , walk, draw"
+myStory = "My name is " + fullName + ".\n " + "I live in " + myCity + ".\n " + "I like " + pastTime + ".";
+myStory = `My name is ${FirstName}. I live in ${myCity}. I like to ${pastTime} `
+console.log(myStory)
+
+
+
+//EX 4 & 5
+
+let language = "JavaScript";
+let createdyear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let selectedSize = "M";
+
+let title = "Name of Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+
+
+
+ 
+//
